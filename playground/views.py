9ee6b0e5405c0e82d84663604be7f3c0cb82173c
Amit Budhathoki <amitbudhathoki30@gmail.com>
2023-05-23 21:28:24 +0545

    using templates

--- playground/views.py
+++ playground/views.py
@@ -6,4 +6,4 @@ def say_hello(request):
 #pull data from db
 # transform     
 #send email
-    return HttpResponse('Hello world')
+   return render(request,'hello.html', { 'name': 'AMIT'}) #parameters (request object ,name of template, put mapping words to change in page)
