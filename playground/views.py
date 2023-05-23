aa027e71214ded8a940671a90f4a69eda21269a6
Amit Budhathoki <amitbudhathoki30@gmail.com>
2023-05-23 19:54:38 +0545

    til mapping urls

--- playground/views.py
+++ playground/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def say_hello(request):
+#pull data from db
+# transform     
+#send email
+    return HttpResponse('Hello world')
