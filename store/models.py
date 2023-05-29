428348a0061cba24326ffcc1c2d4d34401ec076f
Amit Budhathoki <amitbudhathoki30@gmail.com>
2023-05-29 16:25:00 +0545

    done before reverting migrations

--- store/models.py
+++ store/models.py
@@ -39,7 +39,15 @@ class Customer(models.Model):
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True)
-    membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE )
+    membership = models.CharField(
+        max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE )
+    class Meta:
+        db_table = 'store_customers'
+        indexes = [
+            models.Index(fields=['last_name','first_name'])
+        ]
+
+
 
 
 class Order(models.Model):
