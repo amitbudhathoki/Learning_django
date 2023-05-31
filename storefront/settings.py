5f2baaf77387edf55f09a9b0908f71d8ec60618e
Amit Budhathoki <amitbudhathoki30@gmail.com>
2023-05-31 12:16:49 +0545

    deleted migrations

--- storefront/settings.py
+++ storefront/settings.py
@@ -85,13 +85,17 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'storefront',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': '@Aa9843030591'
+
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
