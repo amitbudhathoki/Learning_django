a5a95ab447fce386e896ea57203e505cf19b5a4b
Amit Budhathoki <amitbudhathoki30@gmail.com>
2023-06-01 19:58:34 +0545

    used complex lookups using Q objects

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import Q
+from store.models import Product
 
 
 def say_hello(request):
 #pull data from db
 # transform     
 #send email
-   return render(request,'hello.html', { 'name': 'AMIT'}) #parameters (request object ,name of template, put mapping words to change in page)
+   #keyword = value
+   queryset = Product.objects.filter(Q(inventory__lt=10) | ~Q(umit_price__lt=20))
+   #for product in product:
+      #print (product)
+
+   return render(request,'hello.html', { 'name': 'AMIT', 'products': list(queryset)}) #parameters (request object ,name of template, put mapping words to change in page)
