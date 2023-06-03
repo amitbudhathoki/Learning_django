e5951b159714135c8f0a85ee1a294572099f4a9e
Amit Budhathoki <amitbudhathoki30@gmail.com>
2023-06-03 15:23:30 +0545

    created a custom filter for products

--- store/admin.py
+++ store/admin.py
@@ -1,14 +1,31 @@
+from typing import Any, List, Optional, Tuple
 from django.contrib import admin
 from django.db.models import Count
 from django.db.models.query import QuerySet
+from django.urls import reverse
+from django.utils.html import format_html, urlencode
 from django.http.request import HttpRequest
 from . import models
 # Register your models here.
+
+class InventoryFilter(admin.SimpleListFilter):
+    title = 'inventory'
+    parameter_name = 'inventory'
+
+    def lookups(self, request, model_admin):
+        return [
+            ('<10','Low')
+        ]
+    def queryset(self, request, queryset):
+        if self.value() == '<10':
+           return queryset.filter(inventory__lt=10)
+
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'unit_price', 'inventory_status', 'collection_title']
     list_editable = ['unit_price']
+    list_filter = ['collection', 'last_update', InventoryFilter]
     list_per_page = 20
     list_select_related = ['collection']
 
@@ -32,7 +49,17 @@ class CollectionAdmin(admin.ModelAdmin):
 
     @admin.display(ordering='products_count')
     def products_count(self, collection):
-        return collection.products_count
+       # reverse('admin:app_model_page')
+        url = (
+            reverse('admin:store_product_changelist')
+            + '?'
+            + urlencode({
+                'collection__id': str(collection.id)
+            })
+               )
+
+        return format_html('<a href="{}">{}</a>',url, collection.products_count)
+        
     
     def get_queryset(self, request):
         return super().get_queryset(request).annotate(
@@ -42,10 +69,28 @@ class CollectionAdmin(admin.ModelAdmin):
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ['first_name', 'last_name', 'membership']
+    list_display = ['first_name', 'last_name', 'membership', 'orders_count']
     list_editable = ['membership']
+    list_per_page = 10
     ordering = ['first_name', 'last_name']
-    ist_per_page = 10
+    search_fields = ['first_name__istartswith', 'last_name__istartswith']
+
+    @admin.display(ordering='orders_count')
+    def orders_count(self, customer):
+       # reverse('admin:app_model_page')
+        url = (
+            reverse('admin:store_order_changelist')
+            + '?'
+            + urlencode({'customer__id': str(customer.id)
+            })
+               )
+
+        return format_html('<a href="{}">{}</a>',url, customer.orders_count)
+        
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(
+            orders_count = Count('order')
+        )
 
     
 @admin.register(models.Order)
