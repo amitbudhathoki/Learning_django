7c8bbfa61f602f648a8669d3846b50eb80157898
Amit Budhathoki <amitbudhathoki30@gmail.com>
2023-06-07 21:31:22 +0545

    api view implementation

--- store/views.py
+++ store/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework.decorators import api_view
 
 # Create your views here.
 
+@api_view()
 def product_list(request):
     return HttpResponse('ok')
