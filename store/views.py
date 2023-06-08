721ccd050593e7fac5d93d2af5f3f2acb68aca2e
Amit Budhathoki <amitbudhathoki30@gmail.com>
2023-06-08 21:28:26 +0545

    changed view

--- store/views.py
+++ store/views.py
@@ -7,3 +7,7 @@ from rest_framework.decorators import api_view
 @api_view()
 def product_list(request):
     return HttpResponse('ok')
+
+@api_view()
+def product_list(request):
+    return HttpResponse('ok')
