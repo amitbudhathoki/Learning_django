36b2b0cff4c796f3f7dcad6f5ff9e6ba1f8c7e50
Amit Budhathoki <amitbudhathoki30@gmail.com>
2023-06-24 16:36:50 +0545

    Added changes to Promotion MODEL of store

--- store/models.py
+++ store/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Promotion(models.Model):
     description = models.CharField(max_length=255)
     discount = models.FloatField()
+    #discount = models.FloatField()
 
 
 class Collection(models.Model):
